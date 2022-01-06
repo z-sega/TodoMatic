148cda6cd62335d9287d2d7a46509aa1969f4b0d
Ayodeji Onipe <ayosemail@gmail.com>
2022-01-05 19:48:50 -0500

    Achieved Functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+/* Note on FILTER_MAP & FILTER_NAME:
+ * Defined outside App() because if not they would be recalculated
+ * each the the <App /> component re-renders. 
+ * This information will never change regardless of what the application does. 
+ */
+const FILTER_MAP = {
+	All: () => true, 
+	Active: task => !task.completed,	/* shows tasks whose 'completed' prop is false */
+	Completed: task => task.completed	/* shows tasks whose 'completed' prop is true */
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
+function App(props) {
+	const [tasks, setTasks] = useState(props.tasks);
+	const [filter, setFilter] = useState('All');
+
+	function addTask(name) {
+		const newTask = { id: 'todo-' + nanoid(), name: name, completed: false };
+		setTasks([...tasks, newTask]);
+	}
+
+	function toggleTaskCompleted(id) {
+		const updatedTasks = tasks.map(task => {
+			// if this task has the same ID as the edited task
+			if (id === task.id) {
+				// use object spread to make a new object
+				// whose 'completed' prop has been inverted
+				return {...task, completed: !task.completed};
+			}
+
+			return task;
+		});
+		setTasks(updatedTasks);
+	}
+
+	function deleteTask(id) {
+		const remainingTasks = tasks.filter(task => id !== task.id);
+		setTasks(remainingTasks);
+	}
+
+	function editTask(id, newName) {
+		const editedTaskList = tasks.map(task => { 
+			// if this task has the same ID as the edited task
+			if (id === task.id) {
+				// ...
+				return {...task, name: newName}
+			}
+			
+			return task;
+		});
+		setTasks(editedTaskList);
+	}
+
+	const taskList = tasks
+	.filter(FILTER_MAP[filter])
+	.map(task => (
+			<Todo
+				key={task.id}	/* IMPORTANT to anything rendered with iteration */
+				id={task.id}
+				name={task.name}
+				completed={task.completed}
+				toggleTaskCompleted={toggleTaskCompleted}
+				deleteTask={deleteTask}
+				editTask={editTask}
+			/>
+		)
+	);
+
+	const filterList = FILTER_NAMES.map(name => (
+			<FilterButton 
+				key={name} 
+				name={name} 	
+				isPressed={name === filter}
+				setFilter={setFilter}
+			/>
+		)
+	);
+	
+	const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+	const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+	return (
+		<div className="todoapp stack-large">
+			<h1>TodoMatic</h1>
+			<Form onSubmit={addTask} />
+			<div className="filters btn-group stack-exception">
+				{filterList}
+			</div>
+			<h2 id="list-heading">
+				{headingText}
+			</h2>
+			<ul
+				role="list"
+				className="todo-list stack-large stack-exception"
+				aria-labelledby="list-heading"
+			>
+				{taskList}
+			</ul>
+		</div>
+	);
 }
+
 
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+export default function Form(props) {
+
+    const [name, setName] = useState('');
+
+    function handleChange(e) {
+        setName(e.target.value);        /* <input /> element's value */
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        if (name.length > 0) {
+            props.onSubmit(name);
+        } else {
+            alert("Cannot add empty task!");
+        }
+        setName('');                    /* Good Practice */
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2 className="label-wrapper">
+                <label htmlFor="new-todo-input" className="label__lg">
+                    What needs to be done?
+                </label>
+            </h2>
+            <input
+                type="text"
+                id="new-todo-input"
+                className="input input__lg"
+                name="text"
+                autoComplete="off"
+                value={name}
+                onChange={handleChange}
+            />
+            <button type="submit" className="btn btn__primary btn__lg">
+                Add
+            </button>
+        </form>
+    );
+}
+
