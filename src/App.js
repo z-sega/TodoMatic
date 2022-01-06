fa25dac856495e3e271ce04a4cdc040173423663
Ayodeji Onipe <ayosemail@gmail.com>
2022-01-05 20:48:46 -0500

    title change netlify build test

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ function App(props) {
 
 	return (
 		<div className="todoapp stack-large">
-			<h1>TodoMatic</h1>
+			<h1>TodoMatic!</h1>
 			<Form onSubmit={addTask} />
 			<div className="filters btn-group stack-exception">
 				{filterList}
